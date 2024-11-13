4dc45d9c01624f9dc476316b8fbb6f526dbad5a4
Prathamesh2801 <truepratham2801@gmail.com>
2024-11-13 12:00:41 +0530

    Updated SideBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
 export default function Header() {
+  // Function to close the drawer only on mobile
+  const closeDrawer = () => {
+    if (window.innerWidth < 1024) {
+      const drawerCheckbox = document.getElementById("my-drawer-3");
+      if (drawerCheckbox) drawerCheckbox.checked = false;
+    }
+  };
+
   return (
     <div className="drawer">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
+      <div className="drawer-content flex flex-col transition-transform duration-300 ease-in-out">
         {/* Navbar */}
         <div className="navbar bg-base-200 w-full">
           <div className="flex-none lg:hidden">
@@ -28,8 +37,8 @@ export default function Header() {
               </svg>
             </label>
           </div>
-          <div className="mx-4 flex-1 end-2 px-2 text-3xl font-bold flex justify-end lg:justify-start  ">
-          Vicky  Portfolio
+          <div className="mx-4 flex-1 end-2 px-2 text-3xl font-bold flex justify-end lg:justify-start">
+            Vicky Portfolio
           </div>
           <div className="hidden flex-none lg:block">
             <ul className="menu menu-horizontal space-x-4 text-lg">
@@ -56,31 +65,31 @@ export default function Header() {
           </div>
         </div>
       </div>
-      <div className="drawer-side z-10">
+      <div className="drawer-side z-10 transition-transform duration-300 ease-in-out transform">
         <label
           htmlFor="my-drawer-3"
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 min-h-full w-80 p-4  space-y-4  text-lg">
+        <ul className="menu bg-base-200 min-h-full w-80 p-4 space-y-4 text-lg">
           {/* Sidebar content here */}
           <li>
-            <NavLink to="/">Blog</NavLink>
+            <NavLink to="/" onClick={closeDrawer}>Blog</NavLink>
           </li>
           <li>
-            <NavLink to="/music">Music</NavLink>
+            <NavLink to="/music" onClick={closeDrawer}>Music</NavLink>
           </li>
           <li>
-            <NavLink to="/commercials">Commercials</NavLink>
+            <NavLink to="/commercials" onClick={closeDrawer}>Commercials</NavLink>
           </li>
           <li>
-            <NavLink to="/webseries">Web Series</NavLink>
+            <NavLink to="/webseries" onClick={closeDrawer}>Web Series</NavLink>
           </li>
           <li>
-            <NavLink to="/gallery">Arthouse</NavLink>
+            <NavLink to="/gallery" onClick={closeDrawer}>Arthouse</NavLink>
           </li>
           <li>
-            <NavLink to="/about">About</NavLink>
+            <NavLink to="/about" onClick={closeDrawer}>About</NavLink>
           </li>
         </ul>
       </div>
